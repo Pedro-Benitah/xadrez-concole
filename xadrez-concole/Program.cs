2730942af07d60c4a92fa1f833b644140f4e00d8
Pedro Benitah <pedrobenitah@gmail.com>
2024-01-30 17:44:19 -0300

    comecando metodo que imprime o tabuleiro

--- xadrez-concole/Program.cs
+++ xadrez-concole/Program.cs
@@ -11,11 +11,9 @@ namespace xadrez_concole
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
--- xadrez-concole/tabuleiro/Tabuleiro.cs
+++ xadrez-concole/tabuleiro/Tabuleiro.cs
@@ -3,15 +3,20 @@
     internal class Tabuleiro
     {
 
-        public int linha { get; set; }
+        public int linhas { get; set; }
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro(int linha, int colunas)
+        public Tabuleiro(int linhas, int colunas)
         {
-            this.linha = linha;
+            this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linha, colunas];
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
