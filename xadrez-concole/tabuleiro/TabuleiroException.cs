9b17ff75773ee0b3b9d9fb7aa7d8ca322ba215e5
Pedro Benitah <pedrobenitah@gmail.com>
2024-01-30 18:20:03 -0300

    Criando a classe de posicao para xadrez

--- xadrez-concole/tabuleiro/TabuleiroException.cs
+++ xadrez-concole/tabuleiro/TabuleiroException.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace xadrez_concole.tabuleiro
+namespace tabuleiro
 {
-    internal class TabuleiroException
+    internal class TabuleiroException : Exception
     {
+
+        public TabuleiroException(string msg) : base(msg) { }
     }
 }
