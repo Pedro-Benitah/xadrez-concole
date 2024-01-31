383a50412b2d23a3d052693d1d73924005758823
Pedro Benitah <pedrobenitah@gmail.com>
2024-01-30 21:28:34 -0300

    Implementando a lógica de xequemate

--- xadrez-concole/Program.cs
+++ xadrez-concole/Program.cs
@@ -41,6 +41,8 @@ namespace xadrez_concole
                         Console.ReadLine();
                     }
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch(TabuleiroException e)
             {
